76b8c4b18095128077ff18633636dd7ac930bcae
mirage335 <spamfreemirage335@gmail.com>
2024-12-24 11:29:39 -0500

    Draft.

--- _prog-ops/get-custom.sh
+++ _prog-ops/get-custom.sh
@@ -141,6 +141,167 @@ _get_vmImg_ubDistBuild() {
 	"$scriptAbsoluteLocation" _get_vmImg_ubDistBuild_sequence "$@"
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# DANGER: This is NOT an end user product! No production use!
+# NOTICE: Such discrepancies as mismatch between kernels available during ChRoot and boot cause unusual fragility sometimes necessitating analysis when the resulting image is unbootable.
+_get_vmImg_beforeBoot_ubDistBuild_sequence() {
+	_messageNormal 'init: _get_vmImg_beforeBoot'
+
+	local releaseLabel
+	releaseLabel="internal"
+	[[ "$1" != "" ]] && releaseLabel="$1"
+	[[ "$1" == "latest" ]] && releaseLabel=
+	
+	local functionEntryPWD
+	functionEntryPWD="$PWD"
+	
+	
+	mkdir -p "$scriptLocal"
+	
+	# Only extracted vm img.
+	rm -f "$scriptLocal"/package_image_beforeBoot.tar.flx
+	rm -f "$scriptLocal"/package_image_beforeBoot.tar.flx.part*
+	rm -f "$scriptLocal"/_get/package_image_beforeBoot.tar.flx
+	rm -f "$scriptLocal"/_get/package_image_beforeBoot.tar.flx.part*
+	
+	if [[ -e "$scriptLocal"/vm.img ]]
+	then
+		_messagePlain_good 'good: exists: vm.img'
+		return 0
+	fi
+	
+	if [[ -e "$scriptLocal"/ops.sh ]]
+	then
+		mv -n "$scriptLocal"/ops.sh "$scriptLocal"/ops.sh.bak
+	fi
+	
+	cd "$scriptLocal"
+	mkdir -p "$scriptLocal"/_get
+	cd "$scriptLocal"/_get
+	rm -f "$scriptLocal"/_get/ops.sh
+	export MANDATORY_HASH="true"
+	local currentExitStatus
+	if [[ "$3" == "" ]] # || [[ "$FORCE_AXEL" != "" ]]
+	then
+		_wget_githubRelease_join-stdout "soaringDistributions/ubdist_puddleJumper" "$releaseLabel" "package_image_beforeBoot.tar.flx" | _get_extract_ubDistBuild-tar xv --overwrite
+		currentExitStatus="$?"
+	else
+		#_wget_githubRelease_join-stdout "soaringDistributions/ubdist_puddleJumper" "$releaseLabel" "package_image_beforeBoot.tar.flx" | _get_extract_ubDistBuild-tar --extract vm.img --to-stdout | _dd of="$3" bs=1M
+		_wget_githubRelease_join-stdout "soaringDistributions/ubdist_puddleJumper" "$releaseLabel" "package_image_beforeBoot.tar.flx" | _get_extract_ubDistBuild-tar --extract ./vm.img --to-stdout | sudo -n dd of="$3" bs=1M status=progress
+		currentExitStatus="$?"
+	fi
+	if [[ "$currentExitStatus" != "0" ]]
+	then
+		rm -f "$scriptLocal"/_get/ops.sh
+		_messageFAIL
+	fi
+	export MANDATORY_HASH=
+	unset MANDATORY_HASH
+
+	#if [[ "$3" == "" ]] || [[ "$FORCE_AXEL" != "" ]]
+	#then
+		#true
+	#else
+		#_messagePlain_good 'done: dd: '"$3"
+		#_stop 0
+		#return 0
+	#fi
+	
+	
+	_messagePlain_nominal '_get_vmImg_beforeBoot: hash'
+	
+	if [[ "$FORCE_AXEL" != "" ]] && [[ -e "$scriptLocal"/ops.sh ]]
+	then
+		mv -f "$scriptLocal"/_get/ops.sh "$scriptLocal"/_get/ops.sh.ref
+		rm -f "$scriptLocal"/_get/ops.sh
+	fi
+	
+	local currentHash
+	local currentHash_bytes
+	export MANDATORY_HASH=
+	unset MANDATORY_HASH
+	if [[ "$2" != "" ]]
+	then
+		currentHash="$2"
+		currentHash_bytes=$( [[ "$currentFilePath" != "/dev"* ]] && wc -c "$currentFilePath" )
+	fi
+	if [[ "$currentHash" == "" ]] || [[ "$currentHash_bytes" == "" ]]
+	then
+		currentHash=$(_wget_githubRelease-stdout "soaringDistributions/ubdist_puddleJumper" "$releaseLabel" "_hash-ubdist_beforeBoot.txt" | head -n 3 | tail -n 1)
+		currentHash_bytes=$(_wget_githubRelease-stdout "soaringDistributions/ubdist_puddleJumper" "$releaseLabel" "_hash-ubdist_beforeBoot.txt" | head -n 2 | tail -n 1 | sed 's/^.*count=$(bc <<< '"'"'//' | cut -f1 -d\  )
+	fi
+	( [[ "$currentHash" == "" ]] || [[ "$currentHash_bytes" == "" ]] ) && _messageFAIL
+	export MANDATORY_HASH=
+	unset MANDATORY_HASH
+
+	local currentFilePath
+	currentFilePath="$scriptLocal"/_get/vm.img
+	[[ "$3" != "" ]] && currentFilePath="$3"
+	local currentHashLocal
+	if [[ -e "/etc/ssl/openssl_legacy.cnf" ]]
+	then
+		#currentHashLocal=$(cat "$currentFilePath" | cat | env OPENSSL_CONF="/etc/ssl/openssl_legacy.cnf" openssl dgst -whirlpool -binary | xxd -p -c 256)
+		currentHashLocal=$(dd if="$currentFilePath" bs=1M count=$(bc <<< "$currentHash_bytes"' / 1048576') status=progress | cat | env OPENSSL_CONF="/etc/ssl/openssl_legacy.cnf" openssl dgst -whirlpool -binary | xxd -p -c 256)
+	else
+		#currentHashLocal=$(cat "$currentFilePath" | cat | openssl dgst -whirlpool -binary | xxd -p -c 256)
+		currentHashLocal=$(dd if="$currentFilePath" bs=1M count=$(bc <<< "$currentHash_bytes"' / 1048576') status=progress | cat | openssl dgst -whirlpool -binary | xxd -p -c 256)
+	fi
+	
+	_messagePlain_probe_var currentHash_bytes
+	_messagePlain_probe_var currentHash
+	_messagePlain_probe_var currentHashLocal
+	[[ "$currentHash" != "$currentHashLocal" ]] && _messageFAIL
+	
+	_messagePlain_good 'done: hash'
+	
+	[[ "$3" == "" ]] && mv -f "$scriptLocal"/_get/vm.img "$scriptLocal"/vm.img
+	#mv -f "$scriptLocal"/_get/* "$scriptLocal"/
+	rmdir "$scriptLocal"/_get
+	#_safeRMR "$scriptLocal"/_get
+	
+	cd "$functionEntryPWD"
+}
+_get_vmImg_beforeBoot_ubDistBuild() {
+	"$scriptAbsoluteLocation" _get_vmImg_beforeBoot_ubDistBuild_sequence "$@"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 _get_vmImg_ubDistBuild-live_sequence() {
 	_messageNormal 'init: _get_vmImg-live'
 	
@@ -158,11 +319,19 @@ _get_vmImg_ubDistBuild-live_sequence() {
 	mkdir -p "$scriptLocal"
 	cd "$scriptLocal"
 	export MANDATORY_HASH="true"
+	local currentExitStatus
+	local fallThroughFAIL=FAIL
 	if [[ "$3" == "" ]] # || [[ "$FORCE_AXEL" != "" ]]
 	then
+		fallThroughFAIL=PASS
 		_wget_githubRelease_join "soaringDistributions/ubdist_puddleJumper" "$releaseLabel" "vm-live.iso"
 		currentExitStatus="$?"
-	else
+	fi
+	# Other device names are apparently possible, including rcd, rdvd, svnd0c .
+	#  https://en.wikibooks.org/wiki/Guide_to_Unix/Explanations/Compact_Discs
+	if [[ "$3" == "/dev/sr"* ]] || [[ "$3" == "/dev/dvd"* ]] || [[ "$3" == "/dev/cdrom"* ]]
+	then
+		fallThroughFAIL=PASS
 		currentHash_bytes=$(_wget_githubRelease-stdout "soaringDistributions/ubdist_puddleJumper" "$releaseLabel" "_hash-ubdist.txt" | head -n 14 | tail -n 1 | sed 's/^.*count=$(bc <<< '"'"'//' | cut -f1 -d\  )
 		
 		if ! [[ $(df --block-size=1000000 --output=avail "$tmpSelf" | tr -dc '0-9') -gt "3880" ]]
@@ -195,7 +364,7 @@ _get_vmImg_ubDistBuild-live_sequence() {
 		
 		
 		
-		( _wget_githubRelease_join-stdout "soaringDistributions/ubdist_puddleJumper" "$releaseLabel" "vm-live.iso" | tee >(openssl dgst -whirlpool -binary | xxd -p -c 256 >> "$scriptLocal"/hash-download.txt) ; dd if=/dev/zero bs=2048 count=$(bc <<< '1000000000000 / 2048' ) ) | sudo -n growisofs -speed=2 -dvd-compat -Z "$3"=/dev/stdin -use-the-force-luke=notray -use-the-force-luke=spare:min -use-the-force-luke=bufsize:128m
+		( _wget_githubRelease_join-stdout "soaringDistributions/ubdist_puddleJumper" "$releaseLabel" "vm-live.iso" | tee >(openssl dgst -whirlpool -binary | xxd -p -c 256 >> "$scriptLocal"/hash-download.txt) ; dd if=/dev/zero bs=2048 count=$(bc <<< '1000000000000 / 2048' ) ) | sudo -n growisofs -speed=3 -dvd-compat -Z "$3"=/dev/stdin -use-the-force-luke=notray -use-the-force-luke=spare:min -use-the-force-luke=bufsize:128m
 		currentExitStatus="$?"
 		
 		
@@ -216,6 +385,14 @@ _get_vmImg_ubDistBuild-live_sequence() {
 		#growisofs -M /dev/dvd=/dev/zero
 		#growisofs -M /dev/sr1=/dev/zero -use-the-force-luke=notray
 	fi
+	if [[ "$3" == "/dev/sd"* ]] || [[ "$3" == "/dev/hd"* ]] || [[ "$3" == "/dev/disk/"* ]]
+	then
+		fallThroughFAIL=PASS
+
+		( _wget_githubRelease_join-stdout "soaringDistributions/ubdist_puddleJumper" "$releaseLabel" "vm-live.iso" | tee >(openssl dgst -whirlpool -binary | xxd -p -c 256 >> "$scriptLocal"/hash-download.txt) ; dd if=/dev/zero bs=2048 count=$(bc <<< '1000000000000 / 2048' ) ) | sudo -n dd of="$3" bs=1M status=progress
+		currentExitStatus="$?"
+	fi
+	[[ "$fallThroughFAIL" != "PASS" ]] && _messagePlain_bad 'fail: write device' && _messageFAIL
 	#[[ "$currentExitStatus" != "0" ]] && _messageFAIL
 	if [[ "$currentExitStatus" != "0" ]]
 	then
