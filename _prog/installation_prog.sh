132a44b930f55e6a578765f00554a4553b773447
mirage335 <spamfreemirage335@gmail.com>
2025-01-01 13:11:12 -0500

    Upstream.

--- _prog/installation_prog.sh
+++ _prog/installation_prog.sh
@@ -22,7 +22,7 @@ _gitMad_neighbors_procedure() {
 	cd "$functionEntryPWD"
 }
 _gitMad_neighbors() {
-	"$scriptAbsoluteLocation _gitMad_neighbors "$@""
+	"$scriptAbsoluteLocation" _gitMad_neighbors_procedure "$@"
 }
 
 
